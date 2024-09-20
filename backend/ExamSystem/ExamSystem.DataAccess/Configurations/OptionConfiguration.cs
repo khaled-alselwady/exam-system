2f353d68d46d7bc82c19b8854734a371118b889a
khaled-alselwady <khalidsliman2014@gmail.com>
2024-09-20 23:38:29 +0300

    feat(backend:data-access): add option configuration

---
+++ backend/ExamSystem/ExamSystem.DataAccess/Configurations/OptionConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using ExamSystem.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace ExamSystem.DataAccess.Configurations
+{
+    public class OptionConfiguration : EntityTypeConfiguration<Option>
+    {
+        public OptionConfiguration()
+        {
+            HasKey(s => s.Id);
+
+            Property(s => s.Text)
+                .HasMaxLength(255)
+                .HasColumnType("nvarchar");
+
+            Property(s => s.QuestionId)
+                .IsRequired();
+
+            HasIndex(s => s.QuestionId)
+                .IsUnique();
+        }
+    }
+}
---
+++ backend/ExamSystem/ExamSystem.DataAccess/Configurations/SubjectConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using ExamSystem.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace ExamSystem.DataAccess.Configurations
+{
+    public class SubjectConfiguration : EntityTypeConfiguration<Subject>
+    {
+        public SubjectConfiguration()
+        {
+            HasKey(s => s.Id);
+
+            Property(s => s.Id)
+                 .HasColumnType("tinyint");
+
+            Property(s => s.Name)
+                .IsRequired()
+                .HasMaxLength(50)
+                .HasColumnType("nvarchar");
+        }
+    }
+}
