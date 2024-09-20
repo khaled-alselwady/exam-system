ac9742726e883b40ad9ab1ba570980f3f6812874
khaled-alselwady <khalidsliman2014@gmail.com>
2024-09-20 18:28:39 +0300

    feat(backend:entities): add option class

---
+++ backend/ExamSystem/ExamSystem.Entities/Option.cs
@@ -0,0 +1,9 @@
+﻿namespace ExamSystem.Entities
+{
+    public class Option : BaseEntity
+    {
+        public string Text { get; set; }
+        public int QuestionId { get; set; }
+        public Question Question { get; set; }  
+    }
+}
