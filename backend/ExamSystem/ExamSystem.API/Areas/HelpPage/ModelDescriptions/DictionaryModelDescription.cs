d15294dd031ec57c0db070f3d42dacd5547a9da8
khaled-alselwady <khalidsliman2014@gmail.com>
2024-09-20 03:48:58 +0300

    chore(backend:API): add initial files and configuration

---
+++ backend/ExamSystem/ExamSystem.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ExamSystem.API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ backend/ExamSystem/ExamSystem.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ExamSystem.API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
