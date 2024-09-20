5ec3d8e35a1c7e5135327053cdbf86b808c89618
khaled-alselwady <khalidsliman2014@gmail.com>
2024-09-20 22:23:33 +0300

    feat(backend:entities): add result class

---
+++ backend/ExamSystem/ExamSystem.Entities/Result.cs
@@ -0,0 +1,12 @@
+﻿namespace ExamSystem.Entities
+{
+    public class Result : BaseEntity
+    {
+        public int ExamId { get; set; }
+        public Exam Exam { get; set; }
+
+        public int Score { get; set; }
+
+        public bool IsPassed { get; set; }
+    }
+}
