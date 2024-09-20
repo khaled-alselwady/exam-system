2d4b6fcfe17ecf58b3b431097629c812e87f5333
khaled-alselwady <khalidsliman2014@gmail.com>
2024-09-20 22:21:14 +0300

    feat(backend:entities): add student-answer class

--- backend/ExamSystem/ExamSystem.Entities/Question.cs
+++ backend/ExamSystem/ExamSystem.Entities/Question.cs
@@ -5,8 +5,10 @@ namespace ExamSystem.Entities
     public class Question : BaseEntity
     {
         public string Text { get; set; }
+
         public int SubjectId { get; set; }
         public Subject Subject { get; set; }
+
         public int CorrectOptionId { get; set; }
         public Option CorrectOption { get; set; }
 
---
+++ backend/ExamSystem/ExamSystem.Entities/StudentAnswer.cs
@@ -0,0 +1,14 @@
+﻿namespace ExamSystem.Entities
+{
+    public class StudentAnswer : BaseEntity
+    {
+        public int ExamId { get; set; }
+        public Exam Exam { get; set; }
+
+        public int QuestionId { get; set; }
+        public Question Question { get; set; }  
+
+        public int SelectedOptionId { get; set; }
+        public Option SelectedOption { get; set; }
+    }
+}
