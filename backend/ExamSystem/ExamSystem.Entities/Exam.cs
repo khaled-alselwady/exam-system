34e7c6654d3bcb4f7d1a2016eb860505d43cf99d
khaled-alselwady <khalidsliman2014@gmail.com>
2024-09-20 18:36:47 +0300

    feat(backend:entities): add exam class

---
+++ backend/ExamSystem/ExamSystem.Entities/Exam.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExamSystem.Entities
+{
+    public class Exam : BaseEntity
+    {
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+
+        public int SubjectId { get; set; }
+        public Subject Subject { get; set; }
+
+        public DateTime ExamDate { get; set; }  
+    }
+}
