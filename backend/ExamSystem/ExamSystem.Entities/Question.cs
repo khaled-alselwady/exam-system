0b6ffb93e17e39849eb6bf7d70d697e7e1d6c3ff
khaled-alselwady <khalidsliman2014@gmail.com>
2024-09-20 18:25:39 +0300

    feat(backend:entities): add question class

---
+++ backend/ExamSystem/ExamSystem.Entities/Question.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamSystem.Entities
+{
+    public class Question : BaseEntity
+    {
+        public string Text { get; set; }
+        public int SubjectId { get; set; }
+        public Subject Subject { get; set; }
+        public int CorrectOptionId { get; set; }
+    }
+}
