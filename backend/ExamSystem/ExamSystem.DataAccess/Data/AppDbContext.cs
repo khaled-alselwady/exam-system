e3a4768f7813367043ccc31bf14df432ee3f6d02
khaled-alselwady <khalidsliman2014@gmail.com>
2024-09-20 14:37:08 +0300

    feat(backend:data-access): add data-access project

---
+++ backend/ExamSystem/ExamSystem.DataAccess/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSystem.DataAccess.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected AppDbContext() : base("ExamSystemDbContext")
+        {
+        }
+    }
+}
