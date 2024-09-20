0d105863f4170759c1142c0ff0cc85022f7a57de
khaled-alselwady <khalidsliman2014@gmail.com>
2024-09-21 00:11:04 +0300

    feat(backend:data-access): add question configuration

---
+++ backend/ExamSystem/ExamSystem.DataAccess/Configurations/QuestionConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using ExamSystem.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace ExamSystem.DataAccess.Configurations
+{
+    public class QuestionConfiguration : EntityTypeConfiguration<Question>
+    {
+        public QuestionConfiguration()
+        {
+            HasKey(s => s.Id);
+
+            Property(s => s.Text)
+                .IsRequired()
+                .HasMaxLength(int.MaxValue)
+                .HasColumnType("nvarchar");
+
+            HasIndex(s => s.SubjectId);
+            HasIndex(s => s.CorrectOptionId);
+
+            // question - option
+            HasMany(q => q.Options)
+            .WithRequired(o => o.Question)
+            .HasForeignKey(o => o.QuestionId)
+            .WillCascadeOnDelete(false);
+
+            // question - subject
+            HasRequired(q => q.Subject)
+                .WithMany(s => s.Questions)
+                .HasForeignKey(q => q.SubjectId)
+                .WillCascadeOnDelete(false);
+
+            // question - correctOption
+            HasRequired(q => q.CorrectOption)
+                .WithRequiredDependent(co => co.Question)
+                .Map(m => m.MapKey("CorrectOptionId"));
+        }
+    }
+}
