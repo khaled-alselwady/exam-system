4133f7156f862a6f10251bbec3bc02bad1de708e
khaled-alselwady <khalidsliman2014@gmail.com>
2024-09-23 16:02:19 +0300

    feat(frontend:components): add login component

--- frontend/exam-system/src/app/app.module.ts
+++ frontend/exam-system/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { LoginComponent } from './login/login.component';
+import { QuestionsComponent } from './questions/questions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    QuestionsComponent
   ],
   imports: [
     BrowserModule
